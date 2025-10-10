adba800dd13dfe326930794f4f1ebe128aaf4277
Kamil Wolak <118762973+szachiszta@users.noreply.github.com>
2025-10-10 11:11:54 +0200

    login scheme
    Login scheme added, basic routing

--- app/app.py
+++ app/app.py
@@ -1,14 +1,41 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask
+from flask import render_template, request, redirect, url_for
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from auth.routes import auth_bp
 
+
 # 🔹 Wczytaj .env
 load_dotenv()
 
-app = Flask(__name__)
+app = Flask(__name__ , template_folder='templates')
+
+@app.route("/")
+def home():
+    return redirect(url_for('login'))
+
+# Strona logowania
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        # Na razie tylko testowo — bez logiki logowania czy działa
+        if username == "admin" and password == "admin":
+            return redirect(url_for('homepage'))
+        else:
+            return render_template('login.html', error="Niepoprawne dane logowania")
+
+    return render_template('login.html')
+
+#Strona domowa -> TO DO 
+@app.route('/home')
+def homepage():
+    return render_template('home.html')
+
 
 # 🔹 Ustaw SECRET_KEY z .env
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
@@ -18,6 +45,11 @@ app.config['DEBUG'] = os.getenv("DEBUG") == "True"
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "auth.login"
+
+# Tymczasowy brak użytkownika (zeby buggow nie bylo)
+@login_manager.user_loader
+def load_user(user_id):
+    return None  # jeszcze nie ładujemy użytkowników z bazy
 
 # Bcrypt
 bcrypt = Bcrypt(app)
