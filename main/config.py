e4cc020f8d05042a37fe487cc9765f9ba5ca24ab
Raquerro <166436798+Raquerro@users.noreply.github.com>
2026-06-14 14:25:06 +0200

    railway fix

--- main/config.py
+++ main/config.py
@@ -1,22 +1,25 @@
 import os
-from dotenv import load_dotenv
-
-# Wczytanie pliku .env (poziom wyżej od /main)
-load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev_secret")
     DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-    # Ustawione DB_URL
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or (
-        f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}"
-        f"@localhost:5432/{os.getenv('DB_NAME')}"
-    )
+    db_url = os.getenv("DATABASE_URL")
+
+    if not db_url:
+        raise RuntimeError("DATABASE_URL is not set!")
+
+    # fix Railway / Heroku style URL
+    if db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = db_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
- # Bezpieczeństwo ciasteczek
+    # Security cookies
     SESSION_COOKIE_HTTPONLY = True
-    SESSION_COOKIE_SECURE = False  # True jeśli masz HTTPS
     REMEMBER_COOKIE_HTTPONLY = True
-    REMEMBER_COOKIE_SECURE = False
+
+    # Railway = HTTPS → powinno być True
+    SESSION_COOKIE_SECURE = True
+    REMEMBER_COOKIE_SECURE = True
